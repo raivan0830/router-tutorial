4fb81860e87ae9710a080ed23e4b7ee33a0415c5
Jiwon Kang <raivan0830@gmail.com>
2020-09-28 12:41:36 +0900

    13. routing p352

---
+++ src/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import qs from 'qs'
+
+const About = ({ location }) => {
+    console.log(location)
+
+    const query = qs.parse(location.search, {
+        ignoreQueryPrefix: true
+    })
+    const showDetail = query.detail === "true"
+
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>이 프로젝트는 리엑트 라우터 기초를 실습해 보는 예제 프로젝트 입니다. </p>
+            {showDetail && <p>detaile 값을 true로 설정했군요!</p>}
+
+        </div>
+    );
+};
+
+export default About;
